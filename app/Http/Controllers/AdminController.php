b5657f98693440a930460ea7371858a367d3c360
moyo <moyosxtouch@gmail.com>
2024-01-05 17:00:50 -0600

    create admin login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -10,4 +11,17 @@ class AdminController extends Controller
         return view('admin.index');
     }
     //End Method
+        public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }//End Method
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ require __DIR__.'/auth.php';
 //Admin Group Middleware
 Route::middleware(['auth', 'roles:admin'])->group(function(){
  Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+ Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 }); //End Admin Group Middleware
 
-
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
