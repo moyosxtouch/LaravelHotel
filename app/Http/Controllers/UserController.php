9e849a0a0c5ca9fc44fde31ccae52c7da200f992
moyo <moyosxtouch@gmail.com>
2024-01-17 13:48:31 -0600

    complete user logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,5 +49,18 @@ class UserController extends Controller
         return redirect()->back()->with($notification);
 
     }// End Method
+   public function UserLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
 
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+        $notification = array(
+            'message' => 'User Logout Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
+    }//End Method
 }
