0e9f3ea8cda1cfb14401899d2c8b8e8b3e461f9e
moyo <moyosxtouch.com>
2024-01-17 22:57:58 -0600

    user login and logout create notification

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
-
+use App\Models\User;
 class AuthenticatedSessionController extends Controller
 {
     /**
@@ -28,6 +28,15 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+$id = Auth::user()->id;
+        $profileData = User::find($id);
+        $username = $profileData->name;
+
+        $notification = array(
+            'message' => 'User '.$username.' Login Successfully',
+            'alert-type' => 'info'
+        );
+
         $url='';
         if($request->user()->role==='admin'){
             $url='/admin/dashboard';
@@ -35,7 +44,7 @@ class AuthenticatedSessionController extends Controller
             $url='/dashboard';
         }
 
-        return redirect()->intended($url);
+        return redirect()->intended($url)->with($notification);
     }
 
     /**
